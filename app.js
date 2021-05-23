3f1fd567d6240cf1d77f0531c749eec52a5567b4
Azhia <azhia@Amandas-iMac.local>
2021-05-22 18:28:42 -0700

    adding app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function rantomRGB() {
-    const r = Math.floor(Mth.random() * 256);
-    const g = Math.floor(Mth.random() * 256);
-    const b = Math.floor(Mth.random() * 256);
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`
 }
 
-// To changeeach letter independently
+// To change each letter independently
 const letters = document.querySelectorAll('.letter');
 const intervalId = setInterval(function () {
     for (let letter of letters) {
         letter.style.color = randomRGB();
     }
-}, 2000);
+}, 1000);
